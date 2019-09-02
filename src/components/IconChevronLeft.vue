2fab9e8164e409497631407e1cfdc25ed55b8fd8
James Thomson <james.thomson@brndwgn.com>
2019-09-02 16:22:17 +1000

    Add Carousel component and unit tests

---
+++ src/components/IconChevronLeft.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg width="35.2px" height="88px" viewBox="0 0 12.8 32">
+    <polygon points="12.3,32 12.8,31.5 0.9,16 12.8,0.5 12.3,0 0,16"/>
+  </svg>
+</template>
+
+<style scoped lang="scss">
+  polygon {
+    fill: var(--color-bg);
+  }
+</style>
+
+<script>
+  export default {
+    name: 'IconChevronLeft'
+  };
+</script>
---
+++ src/components/IconChevronRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg width="35.2px" height="88px" viewBox="0 0 12.8 32">
+    <polygon points="0.5,0 0,0.5 11.9,16 0,31.5 0.5,32 12.8,16"/>
+  </svg>
+</template>
+
+<style scoped lang="scss">
+  polygon {
+    fill: var(--color-bg);
+  }
+</style>
+
+<script>
+  export default {
+    name: 'IconChevronRight'
+  };
+</script>
