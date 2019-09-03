18e7fa6f36489a461f33cfdbb35c395a61b64278
James Thomson <james.thomson@brndwgn.com>
2019-09-03 11:01:43 +1000

    Update ThemeToggle unit test spec

--- tests/unit/components/ThemeToggle.spec.js
+++ tests/unit/components/ThemeToggle.spec.js
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import ThemeToggle from '@/components/ThemeToggle.vue';
 
+window.matchMedia = () => ({});
+
 const localVue = createLocalVue();
 
 describe('ThemeToggle', () => {
@@ -13,17 +15,27 @@ describe('ThemeToggle', () => {
     });
   });
 
+  afterEach(() => wrapper.destroy());
+
   it ('Renders the component', () => {
     const el = wrapper.find('.toggle');
 
     expect(el.exists()).to.be.true;
   });
 
-  it ('Toggles to dark theme when clicked', () => {
+  it ('Switches to dark theme', () => {
     const el = wrapper.find('.toggle');
 
-    wrapper.vm.toggle();
+    wrapper.vm.setAsDark();
 
     expect(el.classes('toggle--is-dark')).to.be.true;
+  });
+
+  it ('Switches to light them', () => {
+    const el = wrapper.find('.toggle');
+
+    wrapper.vm.setAsLight();
+
+    expect(el.classes('toggle--is-light')).to.be.true;
   });
 })
